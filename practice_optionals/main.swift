4a0c1f108c0744ae8f68345477fe11a9eb86ab90
Maggie <maggieli1996@gmail.com>
2023-03-12 23:12:55 +0900

    add_optionals

--- practice_optionals/main.swift
+++ practice_optionals/main.swift
@@ -7,5 +7,36 @@
 
 import Foundation
 
-print("Hello, World!")
+enum Genre: String {
+    case country, blues, folk
+}
+
+struct Artist {
+    let name: String
+    var primaryGenre: Genre?
+}
+
+struct Song {
+    let title: String
+    let released: Int
+    let artist: Artist?
+}
+
+//let walkTheLine: Song = Song(title: "stary", released: 32823, artist: Artist(name: "John"))
+let walkTheLine: Song = Song(title: "Walk the Line", released: 32823, artist: Artist(name: "John"))
+
+if let artist = walkTheLine.artist?.name {
+    print(artist)
+} else {
+    print("Artist unknown")
+}
+
+let jack: Artist = Artist(name: "Jack", primaryGenre: nil)
+
+if let primaryGenre = jack.primaryGenre {
+    print(primaryGenre)
+} else {
+    print("Genre unknown")
+}
+
 
